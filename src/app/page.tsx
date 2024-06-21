285e56589b86eaae3d4a04c21f040553b9aaec4c
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-06-21 16:08:42 +0400

    hero and navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
-
+import Navbar from "./components/navbar/page";
+import HeroImg from "../../public/img/Hero section video.png";
 export default function Home() {
-  return <>next</>;
+  return (
+    <>
+      <Navbar />
+      <Image
+        src={HeroImg}
+        alt="Hero Image"
+        style={{ width: "100vw", objectFit: "cover" }}
+      />
+    </>
+  );
 }
